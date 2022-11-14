19bdac094544a38ed25da03accddf9c03bfcdb8a
Jong012 <Jong012>
2022-11-14 18:46:04 +0900

    feat(flooding.models): rainfall_gu_code field 제거

--- backend/flooding/models.py
+++ backend/flooding/models.py
@@ -8,5 +8,4 @@ class SeoulGu(models.Model):
     강우량 CSV 파일과 하수관 수위 정보 CSV 에서 구 이름을 중복제거하여 저장함
     """
     name = models.CharField(max_length=16, unique=True)
-    rainfall_gu_code = models.CharField(max_length=16, blank=True, null=True)
     water_level_gu_code = models.CharField(max_length=16, blank=True, null=True)
